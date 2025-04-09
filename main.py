7a2c2d3c83079b471f251f330b9f9949e14f6702
Bidulusha <74060730+Bidulusha@users.noreply.github.com>
2025-04-09 18:50:13 +0300

    update

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import easyocr
-import os
 
-reader = easyocr.Reader(['ru'])
-
-file_folder = "./cheks"
-
-for  filename in os.listdir(file_folder):
-    if filename.lower().endswith(".jpg"):
-        image_path = os.path.join(file_folder, filename)
-
-        result = reader.readtext(image_path)
-
-        print("Current file is " + image_path)
-
-        for i in result:
-            print(i)
+reader = easyocr.Reader(['ru', 'en'])
+results = reader.readtext('cheks/chek1.jpg')
+    
+for text in results:
+    print(text)
